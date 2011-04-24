fc70b3487b2c63c7b4e859b0ac90e5e7db85459a
Adrian Ulrich <adrian@blinkenlights.ch>
2011-04-24 21:12:01 +0200

    add hintimage

--- src/ch/blinkenlights/battery/BlinkenlightsBattery.java
+++ src/ch/blinkenlights/battery/BlinkenlightsBattery.java
@@ -15,6 +15,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.content.ComponentName;
