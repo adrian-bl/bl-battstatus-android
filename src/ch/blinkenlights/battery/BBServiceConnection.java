ae8be5e04b6dbae849fb739e5aa4002f2b268162
Adrian Ulrich <adrian@blinkenlights.ch>
2011-04-25 09:36:29 +0200

    remove whitespaces

--- src/ch/blinkenlights/battery/BBServiceConnection.java
+++ src/ch/blinkenlights/battery/BBServiceConnection.java
@@ -15,14 +15,14 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 
 public class BBServiceConnection implements ServiceConnection {
-       public BlinkenlightsBatteryService bbsvc;
+	public BlinkenlightsBatteryService bbsvc;
 
-       public void onServiceConnected(ComponentName name, IBinder service) {
-               bbsvc = ((BlinkenlightsBatteryService.LocalBinder) service).getService();
-       }
-
-       public void onServiceDisconnected(ComponentName name) {
-               bbsvc = null;
-       }
+	public void onServiceConnected(ComponentName name, IBinder service) {
+		bbsvc = ((BlinkenlightsBatteryService.LocalBinder) service).getService();
+	}
+	
+	public void onServiceDisconnected(ComponentName name) {
+		bbsvc = null;
+	}
 }
 
