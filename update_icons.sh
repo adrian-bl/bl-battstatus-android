9f1f9333621d46b99bf6dcebbe5debeb1e661593
Adrian Ulrich <adrian@blinkenlights.ch>
2011-06-28 20:08:35 +0200

    fix buildscript

--- update_icons.sh
+++ update_icons.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 (
- rm res/drawable-hdpi/*_*.png &&
- rm res/drawable-hdpi/*.xml && 
+ rm -f res/drawable-hdpi/*_*.png &&
+ rm -f res/drawable-hdpi/*.xml && 
  
  cd scripts &&
  perl gen_images.pl &&
