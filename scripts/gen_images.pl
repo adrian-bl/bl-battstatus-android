6c0b675cdd2a7ec0f83fc90951cc4accbe49bc15
Adrian Ulrich <adrian@blinkenlights.ch>
2011-06-29 22:27:37 +0200

    nicer images

--- scripts/gen_images.pl
+++ scripts/gen_images.pl
@@ -7,10 +7,10 @@ use constant FONTPATH => "/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf";
 my @colors = qw(f02c34 bb382c a1542c a16e2c a1872c 9da12c 81a12c 6ca12c 5aa12c 2da12c 2c91a1);
 
 my $crh  = { size=>72, stroke=>12,  points=>"36,36,28,28" };
-my $wfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, o=>0, invert=>0, name=>'wfb' };
-my $bfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, o=>0, invert=>1, name=>'bfb' };
-my $wfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, o=>0, invert=>0, name=>'wfs' };
-my $bfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, o=>0, invert=>1, name=>'bfs' };
+my $wfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, x=>0, y=>0, invert=>0, name=>'wfb' };
+my $bfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, x=>0, y=>0, invert=>1, name=>'bfb' };
+my $wfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, x=>0, y=>1, invert=>0, name=>'wfs' };
+my $bfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, x=>0, y=>1, invert=>1, name=>'bfs' };
 
 
 draw_circle("cr_h_%03d.png", $crh);
@@ -25,9 +25,9 @@ sub draw_font {
 	
 	for((0..15,100)) { assemble_font(sprintf("digit_c_$conf->{name}_%03d.png",$_), $_, $conf);}
 	
-	$conf->{o} = int($conf->{fsize}/2.8);
+	$conf->{x} = int($conf->{fsize}/2.8);
 	for(0..9 ) { assemble_font(sprintf("digit_l_$conf->{name}_%03d.png",$_), $_, $conf);}
-	$conf->{o} = $conf->{o}*-1;
+	$conf->{x} = $conf->{x}*-1;
 	for(0..9 ) { assemble_font(sprintf("digit_r_$conf->{name}_%03d.png",$_), $_, $conf);}
 }
 
@@ -36,7 +36,8 @@ sub assemble_font {
 	
 	my $im    = Image::Magick->new(size=>$config->{size}."x".$config->{size});
 	my $fsize = ($num == 100 ? $config->{fsize_100} : $config->{fsize});
-	my $xoff  = $config->{o};
+	my $xoff  = $config->{x};
+	my $yoff  = $config->{y};
 	my ($tc_out, $tc_in) = ( $config->{invert} ? ('white', 'black') : ('black' , 'white') );
 	$tc_in = "red" if $num <= 15 && $xoff == 0;
 	
@@ -44,12 +45,12 @@ sub assemble_font {
 
 	foreach my $x (-2..2) {
 		foreach my $y (-2..2) {
-			$im->Annotate(text=>"$num", fill=>$tc_out, font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff+$x, y=>$y);
+			$im->Annotate(text=>"$num", fill=>$tc_out, font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff+$x, y=>$yoff+$y);
 		}
 	}
 	
-	$im->Annotate(text=>"$num", fill=>'grey', font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff, y=>0); # creates somewhat of a gardient
-	$im->Annotate(text=>"$num", fill=>$tc_in, font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff, y=>0);
+	$im->Annotate(text=>"$num", fill=>'grey', font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff, y=>$yoff); # creates somewhat of a gardient
+	$im->Annotate(text=>"$num", fill=>$tc_in, font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff, y=>$yoff);
 	
 	$im->write($out);
 }
