3ce19a8a1645311f288f33dd8e77fe45254b41a2
Adrian Ulrich <adrian@blinkenlights.ch>
2011-06-29 21:43:39 +0200

    assemble fonts via xml

--- scripts/gen_images.pl
+++ scripts/gen_images.pl
@@ -6,35 +6,52 @@ use constant FONTPATH => "/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf";
 
 my @colors = qw(f02c34 bb382c a1542c a16e2c a1872c 9da12c 81a12c 6ca12c 5aa12c 2da12c 2c91a1);
 
-my $crh = { size=>72, stroke=>12,  points=>"36,36,28,28" };
-my $wfb  = { size=>72, fsize=>38, fsize_100=>36, font=>FONTPATH, o=>0, o_10=>0, invert=>0, stroke=>1 };
-my $bfb  = { size=>72, fsize=>38, fsize_100=>36, font=>FONTPATH, o=>0, o_10=>0, invert=>1, stroke=>1 };
-my $wfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, o=>0, o_10=>0, invert=>0, stroke=>1 };
-my $bfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, o=>0, o_10=>0, invert=>1, stroke=>1 };
+my $crh  = { size=>72, stroke=>12,  points=>"36,36,28,28" };
+my $wfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, o=>0, invert=>0, name=>'wfb' };
+my $bfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, o=>0, invert=>1, name=>'bfb' };
+my $wfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, o=>0, invert=>0, name=>'wfs' };
+my $bfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, o=>0, invert=>1, name=>'bfs' };
 
 
 draw_circle("cr_h_%03d.png", $crh);
-draw_font("wfb_%03d.png", $wfb);
-draw_font("wfs_%03d.png", $wfs);
+draw_font($wfb);
+draw_font($wfs);
+draw_font($bfb);
+draw_font($bfs);
 
-draw_font("bfb_%03d.png", $bfb);
-draw_font("bfs_%03d.png", $bfs);
 
 sub draw_font {
-	my($out, $config) = @_;
+	my($conf) = @_;
 	
-	foreach my $num (0..100) {
-		my $fsize = ($num == 100 ? $config->{fsize_100} : $config->{fsize});
-		my $o     = ($num >= 10  ? $config->{o_10}      : $config->{o}    );
-		my $im = Image::Magick->new(size=>$config->{size}."x".$config->{size});
-		my ($tc_out, $tc_in) = ( $config->{invert} ? ('white', 'black') : ('black' , 'white') );
-		$tc_in = "red" if $num <= 15;
-		
-		$im->Read("xc:transparent");
-		$im->Annotate(text=>"$num",, fill=>$tc_in, stroke=>$tc_out, strokewidth=>$config->{stroke}, font=>$config->{font}, pointsize=>$fsize+$config->{stroke}, antialias=>'true', gravity=>"Center", x=>$o, y=>0);
-		
-		$im->write(sprintf($out,$num));
+	for((0..15,100)) { assemble_font(sprintf("digit_c_$conf->{name}_%03d.png",$_), $_, $conf);}
+	
+	$conf->{o} = int($conf->{fsize}/2.8);
+	for(0..9 ) { assemble_font(sprintf("digit_l_$conf->{name}_%03d.png",$_), $_, $conf);}
+	$conf->{o} = $conf->{o}*-1;
+	for(0..9 ) { assemble_font(sprintf("digit_r_$conf->{name}_%03d.png",$_), $_, $conf);}
+}
+
+sub assemble_font {
+	my($out, $num, $config) = @_;
+	
+	my $im    = Image::Magick->new(size=>$config->{size}."x".$config->{size});
+	my $fsize = ($num == 100 ? $config->{fsize_100} : $config->{fsize});
+	my $xoff  = $config->{o};
+	my ($tc_out, $tc_in) = ( $config->{invert} ? ('white', 'black') : ('black' , 'white') );
+	$tc_in = "red" if $num <= 15 && $xoff == 0;
+	
+	$im->Read("xc:transparent");
+
+	foreach my $x (-2..2) {
+		foreach my $y (-2..2) {
+			$im->Annotate(text=>"$num", fill=>$tc_out, font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff+$x, y=>$y);
+		}
 	}
+	
+	$im->Annotate(text=>"$num", fill=>'grey', font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff, y=>0); # creates somewhat of a gardient
+	$im->Annotate(text=>"$num", fill=>$tc_in, font=>$config->{font}, pointsize=>$fsize+0, antialias=>'true', gravity=>"Center", x=>$xoff, y=>0);
+	
+	$im->write($out);
 }
 
 sub draw_circle {
--- scripts/gen_xml.pl
+++ scripts/gen_xml.pl
@@ -1,58 +1,36 @@
 #!/usr/bin/perl
+use strict;
 
-#BlackFontBigCircledHole
-foreach my $num (0..100) {
-	$num = sprintf("%03d",$num);
-	open(X, ">", "bfb_cr_h_$num.xml");
-	print X << "EOF";
-<?xml version="1.0" encoding="utf-8"?>
-<layer-list xmlns:android="http://schemas.android.com/apk/res/android">
-  <item android:drawable="\@drawable/cr_h_$num" />
-  <item android:drawable="\@drawable/bfb_$num" />
-</layer-list>
-EOF
-	close(X);
+foreach my $font_type qw(bfb bfs wfb wfs) {
+	
+	# create special fonts: the red 0-15 and 100
+	foreach my $special_font (0..15,100) {
+		my $outfile = sprintf("${font_type}_%03d.xml",$special_font);
+		write_xml($outfile, sprintf("digit_c_${font_type}_%03d",$special_font));
+	}
+	# normal fonts
+	foreach my $fn (16..99) {
+		my $outfile = sprintf("${font_type}_%03d.xml",$fn);
+		write_xml($outfile, sprintf("digit_r_${font_type}_%03d",int($fn/10)), sprintf("digit_l_${font_type}_%03d",int($fn%10)) );
+	}
+	# circle edition
+	foreach my $c (0..100) {
+		my $outfile = sprintf("${font_type}_cr_h_%03d.xml",$c);
+		write_xml($outfile,  sprintf("cr_h_%03d",$c), sprintf("${font_type}_%03d",$c));
+	}
+	
 }
 
-#BlackFontSmallCircledHole
-foreach my $num (0..100) {
-	$num = sprintf("%03d",$num);
-	open(X, ">", "bfs_cr_h_$num.xml");
-	print X << "EOF";
-<?xml version="1.0" encoding="utf-8"?>
-<layer-list xmlns:android="http://schemas.android.com/apk/res/android">
-  <item android:drawable="\@drawable/cr_h_$num" />
-  <item android:drawable="\@drawable/bfs_$num" />
-</layer-list>
-EOF
-	close(X);
-}
 
-#WhiteFontBigCircledHole
-foreach my $num (0..100) {
-	$num = sprintf("%03d",$num);
-	open(X, ">", "wfb_cr_h_$num.xml");
-	print X << "EOF";
-<?xml version="1.0" encoding="utf-8"?>
-<layer-list xmlns:android="http://schemas.android.com/apk/res/android">
-  <item android:drawable="\@drawable/cr_h_$num" />
-  <item android:drawable="\@drawable/wfb_$num" />
-</layer-list>
-EOF
-	close(X);
+sub write_xml {
+	my($outfile,@parts) = @_;
+	open(XML, ">", $outfile) or die "Could not write to $outfile: $!\n";
+	print XML '<?xml version="1.0" encoding="utf-8"?>'."\n";
+	print XML '<layer-list xmlns:android="http://schemas.android.com/apk/res/android">'."\n";
+	foreach (@parts) {
+		print XML "<item android:drawable=\"\@drawable/$_\" />\n";
+	}
+	print XML "</layer-list>\n";
+	close(XML);
+	
 }
-
-#WhiteFontSmallCircledHole
-foreach my $num (0..100) {
-	$num = sprintf("%03d",$num);
-	open(X, ">", "wfs_cr_h_$num.xml");
-	print X << "EOF";
-<?xml version="1.0" encoding="utf-8"?>
-<layer-list xmlns:android="http://schemas.android.com/apk/res/android">
-  <item android:drawable="\@drawable/cr_h_$num" />
-  <item android:drawable="\@drawable/wfs_$num" />
-</layer-list>
-EOF
-	close(X);
-}
-
