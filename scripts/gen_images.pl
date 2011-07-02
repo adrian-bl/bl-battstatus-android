cee9ace4b467e3f411b8ad28c780b4c1f0301ee5
Adrian Ulrich <adrian@blinkenlights.ch>
2011-07-02 17:57:03 +0200

    again other image parameters

--- scripts/gen_images.pl
+++ scripts/gen_images.pl
@@ -2,15 +2,16 @@
 use strict;
 use Image::Magick;
 
+
 use constant FONTPATH => "/usr/share/fonts/TTF/DejaVuSansCondensed-Bold.ttf";
 
 my @colors = qw(f02c34 bb382c a1542c a16e2c a1872c 9da12c 81a12c 6ca12c 5aa12c 2da12c 2c91a1);
 
-my $crh  = { size=>72, stroke=>12,  points=>"35,36,28,28" };
-my $wfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, x=>0, y=>0, invert=>0, name=>'wfb' };
-my $bfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, x=>0, y=>0, invert=>1, name=>'bfb' };
-my $wfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, x=>0, y=>1, invert=>0, name=>'wfs' };
-my $bfs  = { size=>72, fsize=>28, fsize_100=>28, font=>FONTPATH, x=>0, y=>1, invert=>1, name=>'bfs' };
+my $crh  = { size=>72, stroke=>12,  points=>"36,34,26,26" };
+my $wfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, x=>1, y=>-1, invert=>0, name=>'wfb' };
+my $bfb  = { size=>72, fsize=>36, fsize_100=>34, font=>FONTPATH, x=>1, y=>-1, invert=>1, name=>'bfb' };
+my $wfs  = { size=>72, fsize=>26, fsize_100=>26, font=>FONTPATH, x=>0, y=>-1, invert=>0, name=>'wfs' };
+my $bfs  = { size=>72, fsize=>26, fsize_100=>26, font=>FONTPATH, x=>0, y=>-1, invert=>1, name=>'bfs' };
 
 
 draw_circle("cr_h_%03d.png", $crh);
@@ -23,8 +24,10 @@ draw_font($bfs);
 sub draw_font {
 	my($conf) = @_;
 	
+	$conf->{fulldigit} = 1;
 	for((0..15,100)) { assemble_font(sprintf("digit_c_$conf->{name}_%03d.png",$_), $_, $conf);}
 	
+	$conf->{fulldigit} = 0;
 	$conf->{x} = int($conf->{fsize}/3);
 	for(0..9 ) { assemble_font(sprintf("digit_l_$conf->{name}_%03d.png",$_), $_, $conf);}
 	$conf->{x} = $conf->{x}*-1;
@@ -39,7 +42,7 @@ sub assemble_font {
 	my $xoff  = $config->{x};
 	my $yoff  = $config->{y};
 	my ($tc_out, $tc_in) = ( $config->{invert} ? ('white', 'black') : ('black' , 'white') );
-	$tc_in = "red" if $num <= 15 && $xoff == 0;
+	$tc_in = "red" if $num <= 15 && $config->{fulldigit};
 	
 	$im->Read("xc:transparent");
 
@@ -63,7 +66,6 @@ sub draw_circle {
 		   $idx  = 10 if $num >= 95;
 			 
 		my $cc   = $colors[$idx];
-		
 		my $bump = int(270 - ($num*3.60));
 		   $bump ||= 1;
 		   $bump-- if !$num;
@@ -76,4 +78,3 @@ sub draw_circle {
 	}
 	
 }
-
